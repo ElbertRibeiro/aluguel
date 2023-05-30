b1bf7ee5fc374517e939c0ebbc66490c2fde11bb
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-05-29 21:42:44 -0300

    build: adicionando classe para java

---
+++ aluguel-core/src/main/java/com/nortetec/imovel/Imovel.java
@@ -0,0 +1,56 @@
+package com.nortetec.imovel;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "tab_imovel")
+public class Imovel implements Serializable {
+
+    private static final Serializable serializable = 1L;
+
+    private String endereco;
+    private Double valorAluguel;
+
+    public Imovel(String endereco, Double valorAluguel) {
+        this.endereco = endereco;
+        this.valorAluguel = valorAluguel;
+    }
+
+    public Imovel() {
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Imovel imovel = (Imovel) o;
+
+        if (!endereco.equals(imovel.endereco)) return false;
+        return valorAluguel.equals(imovel.valorAluguel);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = endereco.hashCode();
+        result = 31 * result + valorAluguel.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Imovel{" +
+                "endereco='" + endereco + '\'' +
+                ", valorAluguel=" + valorAluguel +
+                '}';
+    }
+}
