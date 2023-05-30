871d6fc329620a1c0dcc11d378d69b334ff18a0d
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-05-29 21:02:26 -0300

    build: modularizando o projeto

---
+++ aluguel-api/src/main/kotlin/com/nortetec/AluguelApi.java
@@ -0,0 +1,11 @@
+package com.nortetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelApi {
+    public static void main(String[] args) {
+        SpringApplication.run(AluguelApi.class, args);
+    }
+}
