f22fb0a078e0a7a8163024dd8189c691fda6b2db
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-06-03 21:47:17 -0300

    feat: convertendo classe para java

---
+++ aluguel-core/src/main/java/com/nortetec/locatario/Locatario.java
@@ -0,0 +1,90 @@
+package com.nortetec.locatario;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Document(collection = "tab_locarario")
+public class Locatario {
+    @Id
+    private UUID id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Locatario() {
+    }
+
+    public Locatario(UUID id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Locatario locatario = (Locatario) o;
+
+        if (!Objects.equals(id, locatario.id)) return false;
+        if (!Objects.equals(nome, locatario.nome)) return false;
+        if (!Objects.equals(email, locatario.email)) return false;
+        return Objects.equals(telefone, locatario.telefone);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (nome != null ? nome.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (telefone != null ? telefone.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Locatario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
